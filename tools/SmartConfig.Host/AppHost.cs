034806994f69921e91a2d20fa9504e9842fd164d
Javier <javier@queryout.com>
2025-08-27 12:50:11 +1000

    Adding volume

--- tools/SmartConfig.Host/AppHost.cs
+++ tools/SmartConfig.Host/AppHost.cs
@@ -44,6 +44,7 @@ var anythingLlm = builder.AddContainer("anythingllm", "mintplexlabs/anythingllm"
     .WaitFor(mcp)
     .WithEnvironment("OLLAMA_API_BASE", "http://localhost:11434")
     .WithEnvironment("STORAGE_DIR", "/app/server/storage")
+    .WithVolume("anythingllm-storage", "/app/server/storage")
     .WithBindMount("Configurations/anythingllm_mcp_servers.json", "/app/server/storage/plugins/anythingllm_mcp_servers.json")
     .WithBindMount("Configurations/empty.json", "/app/server/storage/plugins/agent-flows/empty.json") // Directory is needed
     .WithBindMount("Configurations/empty.json", "/app/server/storage/plugins/agent-skills/empty.json") // Directory is needed
