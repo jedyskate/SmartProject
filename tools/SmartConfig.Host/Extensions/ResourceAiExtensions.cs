522835a5c99618c97a4358b2985457d60859940d
Javier <javier@queryout.com>
2025-09-15 17:17:01 +1000

    Flagging ai Clients

--- tools/SmartConfig.Host/Extensions/ResourceAiExtensions.cs
+++ tools/SmartConfig.Host/Extensions/ResourceAiExtensions.cs
@@ -7,14 +7,14 @@ public static class ResourceAiExtensions
 {
     public static void AddAiResources(this IDistributedApplicationBuilder builder, IResourceBuilder<ProjectResource> api)
     {
-        if (!bool.Parse(builder.Configuration["SmartConfig:Clients:Ai:Enabled"] ?? "true")) return;
-
+        if (!bool.Parse(builder.Configuration["SmartConfig:Clients:Mcp"] ?? "true")) return;
+        
         // Mcp Server
         var mcp = builder.AddProject<SmartConfig_McpServer>("mcp")
             .WaitFor(api)
             .WithReference(api)
             .WithExternalHttpEndpoints();
-
+        
         // Ollama
         var ollama = builder.AddOllama("ollama")
             .WaitFor(mcp)
@@ -23,47 +23,52 @@ public static class ResourceAiExtensions
             .WithExternalHttpEndpoints()
             .WithDataVolume()
             .AddModel("llama32", "llama3.2:latest");
+        
+        var clients = builder.Configuration.GetSection("SmartConfig:Ai:Clients").Get<string[]>();
 
         // n8n
-        var n8n = builder.AddContainer("n8n", "n8nio/n8n", "latest")
-            .WaitFor(mcp)
-            .WaitFor(ollama)
-            .WithParentRelationship(mcp)
-            .WithHttpEndpoint(port: 5678, targetPort: 5678, name: "n8n-http", isProxied: false)
-            .WithExternalHttpEndpoints()
-            .WithEnvironment("TZ", "Australia/Sydney")
-            .WithEnvironment("GENERIC_TIMEZONE", "Australia/Sydney")
-            .WithEnvironment("N8N_HOST", "localhost:5678")
-            .WithEnvironment("WEBHOOK_URL", "http://localhost:5678")
-            .WithVolume("n8n-storage", "/home/node/.n8n")
-            .WithDefaultAiAgent(builder.Configuration); //TODO::Disable for prod environments
+        if (clients?.Contains("n8n") ?? false)
+            builder.AddContainer("n8n", "n8nio/n8n", "latest")
+                .WaitFor(mcp)
+                .WaitFor(ollama)
+                .WithParentRelationship(mcp)
+                .WithHttpEndpoint(port: 5678, targetPort: 5678, name: "n8n-http", isProxied: false)
+                .WithExternalHttpEndpoints()
+                .WithEnvironment("TZ", "Australia/Sydney")
+                .WithEnvironment("GENERIC_TIMEZONE", "Australia/Sydney")
+                .WithEnvironment("N8N_HOST", "localhost:5678")
+                .WithEnvironment("WEBHOOK_URL", "http://localhost:5678")
+                .WithVolume("n8n-storage", "/home/node/.n8n")
+                .WithDefaultAiAgent(builder.Configuration); //TODO::Disable for prod environments
 
         // AnythingLLM
-        var anythingLlm = builder.AddContainer("anythingllm", "mintplexlabs/anythingllm", "latest")
-            .WaitFor(mcp)
-            .WaitFor(ollama)
-            .WithParentRelationship(mcp)
-            .WithHttpEndpoint(port: 3001, targetPort: 3001, name: "anythingLlm-http", isProxied: false)
-            .WithExternalHttpEndpoints()
-            .WithEnvironment("OLLAMA_API_BASE", "http://localhost:11434")
-            .WithEnvironment("STORAGE_DIR", "/app/server/storage")
-            .WithVolume("anythingllm-storage", "/app/server/storage")
-            .WithBindMount("Volumes/AnythingLLM/anythingllm_mcp_servers.json", "/app/server/storage/plugins/anythingllm_mcp_servers.json")
-            .WithBindMount("Volumes/AnythingLLM/empty.json", "/app/server/storage/plugins/agent-flows/empty.json") // Directory is needed
-            .WithBindMount("Volumes/AnythingLLM/empty.json", "/app/server/storage/plugins/agent-skills/empty.json"); // Directory is needed
+        if (clients?.Contains("anythingLlm") ?? false)
+            builder.AddContainer("anythingllm", "mintplexlabs/anythingllm", "latest")
+                .WaitFor(mcp)
+                .WaitFor(ollama)
+                .WithParentRelationship(mcp)
+                .WithHttpEndpoint(port: 3001, targetPort: 3001, name: "anythingLlm-http", isProxied: false)
+                .WithExternalHttpEndpoints()
+                .WithEnvironment("OLLAMA_API_BASE", "http://localhost:11434")
+                .WithEnvironment("STORAGE_DIR", "/app/server/storage")
+                .WithVolume("anythingllm-storage", "/app/server/storage")
+                .WithBindMount("Volumes/AnythingLLM/anythingllm_mcp_servers.json", "/app/server/storage/plugins/anythingllm_mcp_servers.json")
+                .WithBindMount("Volumes/AnythingLLM/empty.json", "/app/server/storage/plugins/agent-flows/empty.json") // Directory is needed
+                .WithBindMount("Volumes/AnythingLLM/empty.json", "/app/server/storage/plugins/agent-skills/empty.json"); // Directory is needed
 
         // LangGraph
-        builder.AddDockerfile("langgraph", "../../src/SmartConfig.LangGraph")
-            .WaitFor(ollama)
-            .WaitFor(mcp)
-            .WithParentRelationship(mcp)
-            .WithHttpEndpoint(port: 8000, targetPort: 8000, name: "langgraph-http")
-            .WithExternalHttpEndpoints()
-            .WithEnvironment("OLLAMA_BASE_URL", "http://host.docker.internal:11434")
-            // .WithEnvironment("LANGCHAIN_TRACING_V2", "true")
-            // .WithEnvironment("LANGCHAIN_API_KEY", "LANGCHAIN_API_KEY")
-            // .WithEnvironment("LANGCHAIN_PROJECT", "SmartConfig")
-            .WithReference(mcp);
+        if (clients?.Contains("langgraph") ?? false)
+            builder.AddDockerfile("langgraph", "../../src/SmartConfig.LangGraph")
+                .WaitFor(ollama)
+                .WaitFor(mcp)
+                .WithParentRelationship(mcp)
+                .WithHttpEndpoint(port: 8000, targetPort: 8000, name: "langgraph-http")
+                .WithExternalHttpEndpoints()
+                .WithEnvironment("OLLAMA_BASE_URL", "http://host.docker.internal:11434")
+                // .WithEnvironment("LANGCHAIN_TRACING_V2", "true")
+                // .WithEnvironment("LANGCHAIN_API_KEY", "LANGCHAIN_API_KEY")
+                // .WithEnvironment("LANGCHAIN_PROJECT", "SmartConfig")
+                .WithReference(mcp);
     }
 
     private static IResourceBuilder<T> WithDefaultAiAgent<T>(this IResourceBuilder<T> builder,
